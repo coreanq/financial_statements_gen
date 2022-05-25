dd0ebbdcd260e25f160d3904336b02a3055b4293
Charles <coreanq@gmail.com>
2022-05-25 15:36:48 +0900

    기본 test

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # financial_statements 
  - OPENDART 사용하여 [퀀트투자할수있다] 책의 관련 지표 데이터화 
- - <img src="http://image.yes24.com/goods/45504859/XL)" width="40%" height="30%" title="px(픽셀) 크기 설정" alt="RubberDuck"></img>
+ - <img src="http://image.yes24.com/goods/45504859/XL)" width="40%" height="30%" title="px(픽셀) 크기 설정" alt="퀀트투자할수있다"></img>
 
 ## Development Environment
  - Windows 10 64bit
@@ -9,5 +9,7 @@
 ## Features
 
 ## HowTo 
+ - [OPENDART](https://opendart.fss.or.kr/) 에 인증키 신청
+ - user_setting.json 파일 만들어서 'api-key' 값에 인증키 문자열로 저장 
  - python financial_statements_gen.py
 
--- financial_statements_gen/financial_statements_gen.py
+++ financial_statements_gen/financial_statements_gen.py
@@ -1,4 +1,25 @@
+import OpenDartReader
+import sys, json
+
 __vserion__  = "0.1.0"
 
+
 if __name__ == "__main__":
-    print("main")
+
+    user_setting_json = []
+    with open(r"financial_statements_gen\user_setting.json", "r") as json_file:
+        user_setting_json = json.load(json_file)
+
+    # ==== 0. 객체 생성 ====kkkkkkkj
+    # 객체 생성 (API KEY 지정) 
+    api_key = user_setting_json['api_key']
+
+    dart = OpenDartReader(api_key) 
+
+
+    # == 1. 공시정보 검색 ==
+    # 삼성전자 2019-07-01 하루 동안 공시 목록 (날짜에 다양한 포맷이 가능합니다)
+    df = dart.list('005930', end='2019-7-1')
+
+    print(df)
+
